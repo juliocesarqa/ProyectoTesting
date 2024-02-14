82c4509f86fe96cf2036013099c35d969e5985e3
juliocesarqa <juliocesar.el25@hotmail.com>
2024-02-14 11:42:12 -0500

    ABCD

---
+++ src/testing/Prueba.java
@@ -0,0 +1,25 @@
+package testing;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Prueba {
+     public static void main(String[] args) {
+         final String EDGE_DRIVER = "webdriver.edge.driver";
+         final String PATH_DRIVER = "C:\\Users\\JULIO\\Desktop\\drivers\\mozilladriver\\geckodriver.exe";
+         System.setProperty(EDGE_DRIVER, PATH_DRIVER);
+
+
+         WebDriver driver = new FirefoxDriver();
+
+         driver.get("https://wikipedia.com");
+
+
+
+
+     }
+
+   
+}

cb741ea7ab61f074a0318a20e04995d4baa5d51e
juliocesarqa <juliocesar.el25@hotmail.com>
2024-02-16 11:49:26 -0500

    SeleniumSelectors

--- src/testing/Prueba.java
+++ src/testing/Prueba.java
@@ -1,9 +1,16 @@
 package testing;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+
+import java.util.List;
 
 
 public class Prueba {
@@ -21,8 +28,31 @@ public class Prueba {
 
          driver.manage().window().maximize();
 
+         By txtBoxWiki = By.id("searchInput");
+         By btnSearch = By.xpath("//*[@id=\"search-form\"]/fieldset/button/i");
+
+
+         driver.findElement(txtBoxWiki).sendKeys("Calendario gregoriano");
+         driver.findElement(btnSearch).click();
+
+         String titulo = driver.getTitle();
+         String titleExpected = "Calendario gregoriano - Wikipedia, la enciclopedia libre";
+
+         Assert.assertEquals(titulo, titleExpected);
+
+
+         By parrafo = By.xpath("//*[@id=\"Problemas_en_el_calendario\"]");
+
+         List<WebElement> elementos = driver.findElements(parrafo);
+
+         for (WebElement elemento : elementos){ //lee los elementos del List y lo asigna a una variable
+             System.out.print(elemento.getText());
+             Assert.assertEquals("Problemas en el calendario", elemento.getText());
+         }
 
      }
+
+
 
 
 }
